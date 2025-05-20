c2e0dc3fd5dbaeb83724352a4387d65efac08d6b
JoaoHFerreira <jaycops@gmail.com>
2025-05-20 18:06:09 +0100

    Stack implementaion in C

--- rust/simple_stack/src/lib.rs
+++ rust/simple_stack/src/lib.rs
@@ -13,7 +13,10 @@ impl  Stack{
         self.data.push(value);
     }
 
-    
+    pub  fn pop(&mut self) -> Option<i32>{
+        self.data.pop()
+    }
+
 }
 
 #[cfg(test)]
@@ -34,6 +37,30 @@ mod tests {
         assert_eq!(stack.data, vec![41])
     }
 
+    #[test]
+    fn test_pop_return(){
+        let  mut stack = Stack::new();
+        stack.push(1);
+        let stack_pop = stack.pop();
+
+
+        // The thin here, using Some keyword is because Rust enforce type, because in some
+        // cases the result can be None, Some come to solve this issue
+        // in the same way that the Option<i32> handles the return type
+
+        let expeceted_value: Option<i32> = Some(1);
+       
+        assert_eq!(stack_pop, expeceted_value);
+    }
+
+    #[test]
+    fn test_pop_stack(){
+        let  mut stack = Stack::new();
+        stack.push(1);
+        stack.pop();
+       
+        assert_eq!(stack, Stack::new());
+    }
 
 }
 
